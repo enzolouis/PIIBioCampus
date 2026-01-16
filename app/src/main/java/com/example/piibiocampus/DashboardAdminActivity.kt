02a3aef5a289afc1f9caafab76b58d83a260ffef
Enzo Louis <ember.contactme@gmail.com>
2026-01-16 18:50:36 +0100

    Implement admin dashboard interface

---
+++ app/src/main/java/com/example/piibiocampus/DashboardAdminActivity.kt
@@ -0,0 +1,13 @@
+package com.example.piibiocampus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardAdminActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_admin)
+
+    }
+}
