4d11811c7004616973ac798b89f37972f720e340
Zelmine <zelmine31@gmail.com>
2026-01-22 13:39:03 +0100

    Fix : change logo + fix error messages

--- app/src/main/java/com/example/piibiocampus/CreateAccountActivity.kt
+++ app/src/main/java/com/example/piibiocampus/CreateAccountActivity.kt
@@ -2,12 +2,17 @@ package com.example.piibiocampus
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import kotlinx.coroutines.launch
 
 class CreateAccountActivity : AppCompatActivity() {
@@ -16,6 +21,7 @@ class CreateAccountActivity : AppCompatActivity() {
     private var passwordZone: EditText? = null
     private var connectBtn: Button? = null
     private var alreadyAccountBtn: TextView? = null
+    private var errorZone: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,7 @@ class CreateAccountActivity : AppCompatActivity() {
         passwordZone = findViewById<EditText?>(R.id.txtMdp)
         connectBtn = findViewById<Button?>(R.id.btnConnexion)
         alreadyAccountBtn = findViewById<TextView?>(R.id.btnAlreadyAccount)
+        errorZone = findViewById<EditText?>(R.id.errorMsg)
 
         alreadyAccountBtn?.setOnClickListener {
             startActivity(Intent(this, ConnectionActivity::class.java))
@@ -58,10 +65,23 @@ class CreateAccountActivity : AppCompatActivity() {
                     goToMainScreen()
                 }
                 .onFailure { error ->
-                    showToast(
-                        "Erreur lors de la création du compte : ${error.message}",
-                        Toast.LENGTH_LONG
-                    )
+                    Log.e("AUTH_error", "error = $error")
+                    val userMessage = when (val cause = error.cause?: error) {
+                        is FirebaseAuthUserCollisionException ->
+                            "Un compte existe déjà avec cet email."
+                        is FirebaseException -> {
+                            val msg = cause.message ?: ""
+                            when {
+                                msg.contains("PASSWORD_DOES_NOT_MEET_REQUIREMENTS") ->
+                                    "Mot de passe trop faible : 10 caractères minimum, 1 majuscule, 1 chiffre et 1 caractère spécial"
+                                else ->
+                                    "Erreur lors de la création du compte."
+                            }
+                        }
+                        else -> "Erreur lors de la création du compte."
+                    }
+                    errorZone?.text = userMessage
+                    //showToast(userMessage, Toast.LENGTH_LONG)
                 }
         }
     }
