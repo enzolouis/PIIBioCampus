0c1f783bb8d57a99a3d3b7347f142ba56f0b47c8
Enzo LOUIS <65914622+enzolouis@users.noreply.github.com>
2026-02-20 01:21:15 +0100

    feat(search): add link intents from admin dashboard to search users admin

--- app/src/main/java/com/example/piibiocampus/ui/admin/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/piibiocampus/ui/admin/DashboardAdminActivity.kt
@@ -1,6 +1,8 @@
 package com.example.piibiocampus.ui.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.piibiocampus.R
 
@@ -10,5 +12,10 @@ class DashboardAdminActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_admin)
 
+        val btn: Button = findViewById(R.id.btnSearch);
+        btn.setOnClickListener {
+            val intent = Intent(this, SearchUsersAdminActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
