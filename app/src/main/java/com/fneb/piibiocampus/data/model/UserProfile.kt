15e2f7519c146adc0aa1eec7e2c32e0cb16f2d11
Zelmine <zelmine31@gmail.com>
2026-04-03 13:55:18 +0200

    Fix : val to var role

--- app/src/main/java/com/fneb/piibiocampus/data/model/UserProfile.kt
+++ app/src/main/java/com/fneb/piibiocampus/data/model/UserProfile.kt
@@ -6,6 +6,6 @@ data class UserProfile(
     val email: String = "",
     val description: String = "",
     val profilePictureUrl: String = "",
-    val role: String = "",
+    var role: String = "",
     val currentBadge: String = ""
 )
